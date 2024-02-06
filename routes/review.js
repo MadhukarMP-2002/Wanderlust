9bed136fb527b92ecfa7f73aa831d5a4b2de3fc3
MadhukarMP <madhukarmp51@gmail.com>
2024-02-06 07:22:24 +0530

    first

---
+++ routes/review.js
@@ -0,0 +1,27 @@
+const express=require("express");
+const router=express.Router({mergeParams:true});
+const wrapAsync = require("../utils/wrapAsync.js"); 
+const ExpressError = require("../utils/ExpressError.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+const {validateReview, isLoggedIn,isreviewAuthor}=require("../middleware.js");
+const reviewController = require("../controller/review.js");
+
+
+ 
+//Reviews
+//Post route
+
+router.post("/",
+validateReview,
+isLoggedIn,
+wrapAsync(reviewController.createReview));
+
+// Delete Review Route
+
+router.post("/:reviewId",
+isLoggedIn,
+isreviewAuthor,
+wrapAsync(reviewController.destroyReview));
+
+module.exports=router;
